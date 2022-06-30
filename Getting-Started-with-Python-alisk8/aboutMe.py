272a2eff9607c1a52ec899297b63be7d03558dc0
Ali Shadab Khowaja <alishadabkhowaja123@gmail.com>
2022-06-30 11:25:54 -0700

    adding my folder

---
+++ Getting-Started-with-Python-alisk8/aboutMe.py
@@ -0,0 +1,25 @@
+name = "Ali Shadab Khowaja"
+
+nickname = "Ask"
+
+age = 16
+
+used_python_before = True
+
+hobbies = ["watching basketball", "Coding", "playing basketball"]
+
+fav_things = {
+  'Favorite Movie': "Pursuit Of Happyness",
+  'Favorite Sport': "Basketball",
+  'Favorite Food': "Pizza",
+  'Favorite Basketball Team': "Clippers",
+  'Favorite Hobby': hobbies[1]
+}
+
+print(name, nickname, age, used_python_before)
+print()
+print("Hobbies:", hobbies)
+print()
+print("Favorites:", fav_things)
+
+all_vars = dict(vars())
